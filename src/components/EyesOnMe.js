81b07950d7590fbde19ade6a30e4a309be08d43e
Alex McDaniel <alexpmcdaniel@gmail.com>
2022-04-28 00:39:05 -0400

    commit tree eb11695de0c6f431968c5e8728c390d985f58866

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function handlerBlur(){
+    console.log('Hey! Eyes on me!')
+}
+function handlerFocus(){
+    console.log('Good!')
+}
+
+function EyesOnMe() {
+  return (
+    <button onFocus={handlerFocus} onBlur={handlerBlur}>{'Eyes on me'}</button>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+import React from 'react'
+
+function handlerOnPasswordChange(){
+    console.log('Entering password...')
+}
+
+function Keypad() {
+  return (
+    <input type="password" onChange={handlerOnPasswordChange}>
+
+    </input>
+  )
+}
+
+export default Keypad
